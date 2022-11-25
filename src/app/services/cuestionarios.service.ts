818bf8d1473a64cc78906125ac28b2cfa8d5ef14
Manuel <garcia.manuel.b7@gmail.com>
2022-11-24 22:30:59 -0600

    Rename quiz fields

--- src/app/services/cuestionarios.service.ts
+++ src/app/services/cuestionarios.service.ts
@@ -18,43 +18,43 @@ export class CuestionariosService {
   async createQuizz(group: string, name: string): Promise<{ groupId: string, quizzId: string }> {
     let userId = localStorage.getItem('userId')
 
-    let qr = query(collection(this.firestore, 'grupos'),
-      where('creador', '==', userId),
-      where('nombre', '==', group))
+    let qr = query(collection(this.firestore, 'groups'),
+      where('creator', '==', userId),
+      where('group', '==', group))
 
     let docs = await getDocs(qr)
     let groupId = docs.docs[0].id
 
-    let res = await addDoc(collection(this.firestore, `grupos/${groupId}/cuestionarios`), {
-      cuestionario: name,
+    let res = await addDoc(collection(this.firestore, `groups/${groupId}/quizzes`), {
+      quiz: name,
     })
 
     return { groupId: groupId, quizzId: res.id }
   }
 
   async addQuestion(groupId: string, quizzId: string, question: Question) {
-    await addDoc(collection(this.firestore, `grupos/${groupId}/cuestionarios/${quizzId}/preguntas`), {
-      pregunta: question.question, 
-      descripcion: question.description, 
-      respuestaCorrecta: question.correctAnswer, 
-      respuestas: question.answers, 
+    await addDoc(collection(this.firestore, `groups/${groupId}/quizzes/${quizzId}/questions`), {
+      question: question.question, 
+      description: question.description, 
+      correctAnswer: question.correctAnswer, 
+      answers: question.answers, 
     })
   }
 
   async getQuizz(groupId: string, quizzId: string): Promise<Quizz> {
-    let document = await getDoc(doc(this.firestore, `grupos/${groupId}/cuestionarios/${quizzId}`))
+    let document = await getDoc(doc(this.firestore, `groups/${groupId}/quizzes/${quizzId}`))
 
-    let name = document.get('cuestionario') as string
+    let name = document.get('quiz') as string
 
-    let qDocuments = await getDocs(collection(this.firestore, `grupos/${groupId}/cuestionarios/${quizzId}/preguntas`))
+    let qDocuments = await getDocs(collection(this.firestore, `groups/${groupId}/quizzes/${quizzId}/questions`))
 
     let questions = qDocuments.docs.map((e) => {
 
       let questionId = e.id
-      let question = e.get('pregunta') as string
-      let description = e.get('descripcion') as string
-      let correctAnswer = e.get('respuestaCorrecta') as string
-      let answers = e.get('respuestas') as Object
+      let question = e.get('question') as string
+      let description = e.get('description') as string
+      let correctAnswer = e.get('correctAnswer') as string
+      let answers = e.get('answers') as Object
 
       return <Question>{ 
         questionId,
